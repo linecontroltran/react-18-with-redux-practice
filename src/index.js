7742aeef0b94713921e59905074cb768248ff62b
Tom <linecontroltran@gmail.com>
2023-09-13 18:51:40 -0400

    Version 2 props work

--- src/index.js
+++ src/index.js
@@ -3,45 +3,36 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const author = "Walter Isaacson";
+const title = "Elon Musk";
+const img = "./images/book1.jpg";
+
+//paramters
+const someFunc = (param1, param2) => {
+  console.log(param1, param2);
+};
+//arguments
+someFunc("job", "developer");
+
 const Booklist = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
+      <Book author={author} />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL900_SR900,600_.jpg"
-      alt="elon musk"
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>Elon Musk</h1>;
-};
-
-const Author = () => {
-  return <h4>Walter Isaacson </h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
