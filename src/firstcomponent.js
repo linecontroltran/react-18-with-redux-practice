ebb47029f8b826c944fbf1d913d412aaf93db98d
Tom <linecontroltran@gmail.com>
2023-09-13 18:17:34 -0400

    Version 1, React 18 with redux practice

---
+++ src/firstcomponent.js
@@ -0,0 +1,19 @@
+// #1 THIS IS FIRST COMPONENT
+
+/*
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Greeting() {
+  return <h2>My First Component</h2>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);
+
+*/
+
+//npm start ctrl + c
+//everytime you think of component THINK FUNCTION
--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
+
+const Book = () => {
+  return (
+    <article className="book">
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/814mI0-rkxL._AC_UL900_SR900,600_.jpg"
+      alt="elon musk"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>Elon Musk</h1>;
+};
+
+const Author = () => {
+  return <h4>Walter Isaacson </h4>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Booklist />);
