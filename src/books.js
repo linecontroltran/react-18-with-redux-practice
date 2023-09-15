abb39976e87c8b0a84cf3f4f0dc5e45cd1642dd6
Tom <linecontroltran@gmail.com>
2023-09-15 19:15:26 -0400

    version 15, images in the src folder, import to books.js

--- src/books.js
+++ src/books.js
@@ -1,14 +1,25 @@
+import book1 from "./images/book1.jpg";
+import book2 from "./images/book2.jpg";
+import book3 from "./images/book3.jpg";
+
 export const books = [
   {
     author: "Walter Isaacson",
     title: "Elon Musk",
-    img: "./images/book1.jpg",
+    img: book1,
     id: 1,
   },
   {
     author: "Arthur C. Brooks, And Oprah",
     title: "Build the Life You Want: The Art and Science of Getting Happier",
-    img: "./images/book2.jpg",
+    img: book2,
     id: 2,
+  },
+  {
+    author: "Keila Shaheen",
+    title:
+      "The Shadow Work Journal: A Guide to Integrate and Transcend your Shadows",
+    img: book3,
+    id: 3,
   },
 ];
