60b0b4f033f7995d45f997ce34e131b6688557e7
Tom <linecontroltran@gmail.com>
2023-09-15 15:29:28 -0400

    Version 10, event handlers

--- src/index.js
+++ src/index.js
@@ -31,19 +31,27 @@ const Booklist = () => {
 };
 
 const EventExamples = () => {
-  const handleFormInput = () => {
-    console.log("handle form input");
+  const handleFormInput = (e) => {
+    //console.log(e);
+    console.log(e.target);
+    console.log(e.target.name);
+    console.log(e.target.value);
   };
   const handleButtonClick = () => {
     alert("handle button click");
   };
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+  };
+
   return (
     <section>
-      <form>
+      <form onSubmit={handleFormSubmission}>
         <h2>typical form</h2>
         <input
           type="text"
-          name="example"
+          name="product"
           onChange={handleFormInput}
           style={{ margin: "1rem 0" }}
         />
